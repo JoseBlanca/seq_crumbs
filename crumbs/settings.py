c1464f9032f6e5b17f56200db76b3c1b18bd9761
peio <pziarsolo@upv.es>
2013-02-18 15:16:07 +0100

    fix settigs TEMPDIR

--- crumbs/settings.py
+++ crumbs/settings.py
@@ -93,7 +93,7 @@ _DUST_WINDOWSIZE = 64
 _DUST_WINDOWSTEP = 32
 _DEFATULT_DUST_THRESHOLD = 7
 
-_TEMP_DIR = '/home/peio/work_in/bug_parallel/memdir'
+_TEMP_DIR = None 
 
 # min_mapq to use as a filter for maped reads
 _DEFAULT_MIN_MAPQ = 0
