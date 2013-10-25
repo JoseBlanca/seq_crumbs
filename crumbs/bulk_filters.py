7cc4d4eb9053f38d8403ef8e9182c62b9c9b8417
carlos <carlos199m1@gmail.com>
2013-10-25 15:18:03 +0200

    half baked bulk duplicate filter implemented

---
+++ crumbs/bulk_filters.py
@@ -0,0 +1,152 @@
+# Copyright 2012 Jose Blanca, Peio Ziarsolo, COMAV-Univ. Politecnica Valencia
+# This file is part of seq_crumbs.
+# seq_crumbs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# seq_crumbs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with seq_crumbs. If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=C0111
+
+from subprocess import Popen, PIPE
+import sys
+from itertools import imap
+from tempfile import NamedTemporaryFile
+
+from crumbs.seq import get_str_seq, get_title, get_str_qualities
+from crumbs.pairs import group_seqs_in_pairs
+from crumbs.seqio import read_seqs
+from crumbs.utils.tags import SEQITEM, GUESS_FORMAT
+from crumbs.utils.file_utils import flush_fhand
+from crumbs.iterutils import group_in_packets_fill_last
+from crumbs.seq import SeqItem, SeqWrapper
+from crumbs.exceptions import UnknownFormatError
+
+
+def _tabbed_pairs_equal(pair1, pair2):
+    pair1 = pair1.split('\t')
+    pair2 = pair2.split('\t')
+    if len(pair1) != len(pair2):
+        return False
+
+    seqs_in_pair1 = pair1[1::3]
+    seqs_in_pair2 = pair2[1::3]
+    for read1, read2 in zip(seqs_in_pair1, seqs_in_pair2):
+        if read1 != read2:
+            return False
+    return True
+
+
+def _seqitem_pairs_equal(pair1, pair2):
+    if len(pair1) != len(pair2):
+        return False
+    elif len(pair1) == 2:
+        for read1, read2 in zip(pair1, pair2):
+            if not get_str_seq(read1) == get_str_seq(read2):
+                return False
+        return True
+    else:
+        return get_str_seq(pair1) == get_str_seq(pair2)
+
+def _seq_to_tabbed_str(read):
+    title = '@' + get_title(read)
+    title = title.replace('\t', ' ')
+    sequence = get_str_seq(read)
+    quality = get_str_qualities(read)
+    return '\t'.join([title, sequence, quality])
+
+
+def _tabbed_pair_to_seqs_seqitem(pair_line, file_format):
+    pair_items = pair_line.rstrip().split('\t')
+
+    if 'fastq' not in file_format:
+        raise NotImplemented('Not implemented yet for fasta')
+    seqs = []
+    for read_items in group_in_packets_fill_last(pair_items, 3):
+        title = read_items[0]
+        lines = [read_items[0]+'\n', read_items[1]+'\n', '+\n', read_items[2]+'\n']
+        seqitem = SeqItem(title.split()[0][1:], lines)
+        seq = SeqWrapper(SEQITEM, seqitem, file_format)
+        seqs.append(seq)
+    return seqs
+
+
+def _pair_to_tabbed_str(pair):
+    reads = [_seq_to_tabbed_str(read) for read in pair]
+    reads.append('\n')
+    return '\t'.join(reads)
+
+
+def _read_pairs(in_fhands, file_format=GUESS_FORMAT):
+    seqs = read_seqs(in_fhands, file_format=file_format,
+                     prefered_seq_classes=[SEQITEM])
+    seqs = list(seqs)
+    pairs = group_seqs_in_pairs(seqs)
+    return pairs
+
+
+def _convert_fastq_to_tabbed_pairs(in_fhands, out_fhand,
+                                   file_format=GUESS_FORMAT):
+    'It converts fastq files to one line per pair format'
+    pairs = _read_pairs(in_fhands, file_format=file_format)
+    pairs_in_one_line = imap(_pair_to_tabbed_str, pairs)
+    for pair in pairs_in_one_line:
+        try:
+            out_fhand.write(pair)
+        except IOError, error:
+            # The pipe could be already closed
+            if not 'Broken pipe' in str(error):
+                raise
+    flush_fhand(out_fhand)
+
+
+CONVERT_FASTQ_TO_LINES_SCRIPT = '''
+from sys import argv, stdout
+from crumbs.bulk_filters import _convert_fastq_to_tabbed_pairs
+in_fpaths = argv[1:]
+in_fhands = [open(fpath) for fpath in in_fpaths]
+_convert_fastq_to_tabbed_pairs(in_fhands, out_fhand=stdout)
+'''
+
+
+def filter_duplicates(in_fpaths, in_format=GUESS_FORMAT):
+    if not in_fpaths:
+        raise ValueError('At least one input fpath is required')
+    for fpath in in_fpaths:
+        fhand = open(fpath)
+        if fhand.next() == '':
+            raise UnknownFormatError('The file is empty')
+        fhand.close()
+    file_format = in_format
+    to_lines_script = NamedTemporaryFile(prefix='fastq_to_lines.',
+                                         suffix='.py')
+    to_lines_script.write(CONVERT_FASTQ_TO_LINES_SCRIPT)
+    to_lines_script.flush()
+    to_lines_cmd = [sys.executable, to_lines_script.name]
+    to_lines_cmd.extend(in_fpaths)
+
+    to_lines = Popen(to_lines_cmd, stdout=PIPE)
+    sort = Popen(['sort', '-k', '2,5', '-T', '-u'],
+                 stdin=to_lines.stdout, stdout=PIPE)
+
+    prev_pair = None
+    for pair_line in sort.stdout:
+        if prev_pair == None:
+            duplicated = False
+        else:
+            duplicated = _tabbed_pairs_equal(pair_line, prev_pair)
+        if not duplicated:
+            yield _tabbed_pair_to_seqs_seqitem(pair_line,
+                                               file_format=file_format)
+        prev_pair = pair_line
+
+    to_lines_script.close()
+    #print to_lines.returncode
+    #print sort.returncode
---
+++ test/test_bulk_filters.py
@@ -0,0 +1,218 @@
+# Copyright 2012 Jose Blanca, Peio Ziarsolo, COMAV-Univ. Politecnica Valencia
+# This file is part of seq_crumbs.
+# seq_crumbs is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# seq_crumbs is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR  PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with seq_crumbs. If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=R0201
+# pylint: disable=R0904
+# pylint: disable=W0402
+# pylint: disable=C0111
+
+
+import unittest
+from tempfile import NamedTemporaryFile
+from StringIO import StringIO
+
+from crumbs.seq import (SeqWrapper, SeqItem, get_title,
+                        get_str_seq, get_str_qualities)
+from crumbs.utils.tags import SEQITEM
+from crumbs.bulk_filters import (filter_duplicates, _tabbed_pairs_equal,
+                                 _read_pairs, _convert_fastq_to_tabbed_pairs,
+                                 _tabbed_pair_to_seqs_seqitem,
+                                 _pair_to_tabbed_str, _seqitem_pairs_equal)
+
+FASTQ_NO_DUPS1 = '''@CUESXEL822 1:Y:18:ATCACG
+TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT
++
+AEGDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@
+@CUESXEL822 2:Y:18:ATCACG
+TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+@CUESXEL824 1:Y:18:ATCACG
+GATTGAAGCTCCAAACCGCCATGTTCACCACCGCAAGC
++
+HHGEHD8EEHHHDGHHHHHHHHHCEHHHHDHHHHEHHH
+@CUESXEL824 2:Y:18:ATCACG
+TGCTTGCTGCACTTTGATGTTATTATCTGTGTTGTGTT
++
+AA=AF7CDEDAFFDF@5D>D;FCF;GGGDGGEGGGFGE
+'''
+FASTQ_NO_DUPS2 = '''@CUESXEL830 1:Y:18:ATCACG
+CGCTCTTCTTCGCCACCGTGTTCTTGATATCGGCCTTC
++
+HHHHHHHHHHHHHHIHHHHHHGGHHHFHHHHHHHEHHH
+@CUESXEL830 2:Y:18:ATCACG
+TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT
++
+AEGDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@
+@CUESXEL832 1:Y:18:ATCACG
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+@CUESXEL832 2:Y:18:ATCACG
+TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+@CUESXEL834 1:Y:18:ATCACG
+TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+@CUESXEL834 2:Y:18:ATCACG
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+'''
+FASTQ_DUPS = '''@CUESXEL836 1:Y:18:ATCACG
+TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT
++
+TTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@
+@CUESXEL836 2:Y:18:ATCACG
+TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC
++
+ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+'''
+TABBED_PAIR1 = '''@CUESXEL836 1:Y:18:ATCACG\tTAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\tTTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\t@CUESXEL836 2:Y:18:ATCACG\tTCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\tABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n'''
+TABBED_PAIR2 = '''@CUESXEL834 1:Y:18:ATCACG\tTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT\tCG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\t@CUESXEL834 2:Y:18:ATCACG\tAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\tCG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n'''
+
+FASTQ_PAIR = '''@CUESXEL834 1:Y:18:ATCACG
+TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+@CUESXEL834 2:Y:18:ATCACG
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
++
+CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
+'''
+
+
+class FilterDuplicatesTest(unittest.TestCase):
+
+    def test_tabbed_pairs_equal(self):
+        seq1 = '@seq1\tTAATAC\tTTTDFG'
+        seq2 = '@seq2\tTCATTA\tABCBEG'
+        seq3 = '@seq3\tTAATAC\tAGKDFG'
+        seq4 = '@seq4\tACGCGT\tASDJFG'
+
+        pair1 = '\t'.join([seq1, seq2])
+        pair2 = '\t'.join([seq2, seq4])
+        pair3 = '\t'.join([seq3, seq2])
+        pair4 = '\t'.join([seq2, seq1])
+
+        assert _tabbed_pairs_equal(pair1, pair3)
+        assert _tabbed_pairs_equal(pair1, pair2) == False
+        assert _tabbed_pairs_equal(pair1, pair4) == False
+        assert _tabbed_pairs_equal(seq1, seq3)
+        assert _tabbed_pairs_equal(seq1, seq2) == False
+        assert _tabbed_pairs_equal(seq1, pair1) == False
+        assert _tabbed_pairs_equal(pair1, seq2) == False
+
+    def test_seqitem_pairs_equal(self):
+        seq1 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
+                            ['@CUESXEL836 1:Y:18:ATCACG\n',
+                             'TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\n', '+\n',
+                             'TTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\n']),
+                          'fastq')
+        seq2 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
+                            ['@CUESXEL836 2:Y:18:ATCACG\n',
+                             'TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\n', '+\n',
+                             'ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n']),
+                          'fastq')
+
+    def test_pair_to_tabbed_str(self):
+        expected_tabbed_pair = TABBED_PAIR1.rstrip()
+        seq1 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
+                            ['@CUESXEL836 1:Y:18:ATCACG\n',
+                             'TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\n', '+\n',
+                             'TTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\n']),
+                          'fastq')
+        seq2 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
+                            ['@CUESXEL836 2:Y:18:ATCACG\n',
+                             'TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\n', '+\n',
+                             'ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n']),
+                          'fastq')
+        pair = (seq1, seq2)
+        tabbed_pair = _pair_to_tabbed_str(pair).rstrip()
+        #print tabbed_pair
+        #print expected_tabbed_pair
+        assert expected_tabbed_pair == tabbed_pair
+
+    def test_fastq_to_tabbed_pairs(self):
+        fastq1 = FASTQ_DUPS
+        fastq2 = FASTQ_PAIR
+        in_fhand1 = StringIO(fastq1)
+        in_fhand2 = StringIO(fastq2)
+        out_fhand = StringIO()
+        out_fhand2 = StringIO()
+        _convert_fastq_to_tabbed_pairs([in_fhand1, in_fhand2], out_fhand)
+        _convert_fastq_to_tabbed_pairs([in_fhand1], out_fhand2)
+        lines2 = out_fhand.getvalue().splitlines()
+        lines = out_fhand.getvalue().splitlines()
+        expected_lines = [TABBED_PAIR1, TABBED_PAIR2]
+        assert len(lines) == 2
+        assert lines[0].strip() == expected_lines[0].strip()
+        assert lines[1].strip() == expected_lines[1].strip()
+        assert lines[0].strip() == lines2[0].strip()
+
+    def test_line_to_seqitem(self):
+        tabbed_pair = TABBED_PAIR1
+        seq1 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
+                            ['@CUESXEL836 1:Y:18:ATCACG\n',
+                             'TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\n', '+\n',
+                             'TTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\n']),
+                          'fastq')
+        seq2 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
+                            ['@CUESXEL836 2:Y:18:ATCACG\n',
+                             'TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\n', '+\n',
+                             'ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n']),
+                          'fastq')
+        expected_seqs = [seq1, seq2]
+        seqs = _tabbed_pair_to_seqs_seqitem(tabbed_pair, 'fastq')
+        for read1, read2 in zip(seqs, expected_seqs):
+            assert get_title(read1) == get_title(read2)
+            assert get_str_seq(read1) == get_str_seq(read2)
+            assert get_str_qualities(read1) == get_str_qualities(read2)
+
+    def test_filter_duplicates(self):
+        in_fhand = NamedTemporaryFile()
+        fastq_with_dups = FASTQ_NO_DUPS1 + FASTQ_DUPS + FASTQ_NO_DUPS2
+        #print fastq_with_dups
+        in_fhand.write(fastq_with_dups)
+        in_fhand.flush()
+        filtered_pairs = list(filter_duplicates([in_fhand.name], 'fastq'))
+
+        fastq_no_dups = FASTQ_NO_DUPS1 + FASTQ_NO_DUPS2
+        expected_pairs = list(_read_pairs([StringIO(fastq_no_dups)]))
+        #print 'filtered_pairs ->', filtered_pairs
+        #print 'expected_pairs ->', expected_pairs
+        assert len(filtered_pairs) == len(expected_pairs)
+        for pair1 in expected_pairs:
+            counts = 0
+            for pair2 in filtered_pairs:
+                if _seqitem_pairs_equal(pair1, pair2):
+                    counts += 1
+            assert counts == 1
+        in_fhand.close()
+
+    def test_no_pairs(self):
+        in_fhand1 = NamedTemporaryFile()
+        in_fhand2 = NamedTemporaryFile()
+        filtered_pairs = list(filter_duplicates([in_fhand1.name, in_fhand2.name], 'fastq'))
+        in_fhand1.close()
+        in_fhand2.close()
+        assert not filtered_pairs
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'FilterDuplicatesTest.test_no_pairs']
+    unittest.main()
