9a2c2d9b4398ddf79870b5822d41d1ab66150c16
carlos <carlos199m1@gmail.com>
2013-11-07 11:56:06 +0100

    filter_duplicates and test updated with new sorted_unique_items funtion

--- crumbs/bulk_filters.py
+++ crumbs/bulk_filters.py
@@ -15,19 +15,11 @@
 
 # pylint: disable=C0111
 
-from subprocess import Popen, PIPE
-import sys
-from itertools import imap
-from tempfile import NamedTemporaryFile
-
-from crumbs.seq import (get_str_seq, get_title, get_str_qualities,
-                        get_file_format)
+from crumbs.seq import get_str_seq
 from crumbs.pairs import group_seqs_in_pairs
-from crumbs.seqio import read_seqs
+from crumbs.seqio import read_seqs, write_seqs
 from crumbs.utils.tags import SEQITEM
-from crumbs.utils.file_formats import get_format
-from crumbs.iterutils import group_in_packets_fill_last
-from crumbs.seq import SeqItem, SeqWrapper
+from crumbs.iterutils import sorted_unique_items
 
 
 def _tabbed_pairs_equal(pair1, pair2):
@@ -53,49 +45,6 @@ def _seqitem_pairs_equal(pair1, pair2):
         return True
 
 
-def _seq_to_tabbed_str(read):
-    if 'fastq' in get_file_format(read):
-        title = '@' + get_title(read)
-        title = title.replace('\t', ' ')
-        sequence = get_str_seq(read)
-        quality = get_str_qualities(read)
-        return '\t'.join([title, sequence, quality])
-    elif 'fasta' in get_file_format(read):
-        title = '>' + get_title(read)
-        title = title.replace('\t', ' ')
-        sequence = get_str_seq(read)
-        return '\t'.join([title, sequence])
-
-
-def _tabbed_pair_to_seqs_seqitem(pair_line, file_format):
-    pair_items = pair_line.rstrip().split('\t')
-    seqs = []
-    if 'fasta' in file_format:
-        for read_items in group_in_packets_fill_last(pair_items, 2):
-            title = '>' + read_items[0][1:]
-            lines = [title + '\n', read_items[1] + '\n']
-            seqitem = SeqItem(title.split()[0][1:], lines)
-            seq = SeqWrapper(SEQITEM, seqitem, file_format)
-            seqs.append(seq)
-    elif 'fastq' in file_format:
-        for read_items in group_in_packets_fill_last(pair_items, 3):
-            title = read_items[0]
-            lines = [read_items[0] + '\n', read_items[1] + '\n',
-                     '+\n', read_items[2] + '\n']
-            seqitem = SeqItem(title.split()[0][1:], lines)
-            seq = SeqWrapper(SEQITEM, seqitem, file_format)
-            seqs.append(seq)
-    else:
-        raise ValueError('Format not supported')
-    return seqs
-
-
-def _pair_to_tabbed_str(pair):
-    reads = [_seq_to_tabbed_str(read) for read in pair]
-    reads.append('\n')
-    return '\t'.join(reads)
-
-
 def _read_pairs(in_fhands, paired_reads):
     seqs = read_seqs(in_fhands, prefered_seq_classes=[SEQITEM])
     if paired_reads:
@@ -105,79 +54,20 @@ def _read_pairs(in_fhands, paired_reads):
     return pairs
 
 
-def _convert_fastq_to_tabbed_pairs(in_fhands, paired_reads):
-    'It converts fastq files to one line per pair format'
-    pairs = _read_pairs(in_fhands, paired_reads)
-    pairs_in_one_line = imap(_pair_to_tabbed_str, pairs)
-    for pair in pairs_in_one_line:
-        yield pair
+def _key(pair):
+    key = []
+    for read in pair:
+        key.append(get_str_seq(read))
+    return key
 
 
-CONVERT_FASTQ_TO_LINES_SCRIPT = '''
-from sys import argv, stdout
-from crumbs.bulk_filters import _convert_fastq_to_tabbed_pairs
-in_fpaths = argv[1:]
-in_fhands = [open(fpath) for fpath in in_fpaths]
-_convert_fastq_to_tabbed_pairs(in_fhands, out_fhand=stdout)
-'''
-UNIQUE_SEQITEM_SCRIPT = '''
-import sys
-from crumbs.bulk_filters import _unique_and_to_pairs
-from crumbs.seqio import write_seqs
-in_fhand = sys.stdin
-out_format = sys.argv[1]
-filtered_pairs = _unique_and_to_pairs(in_fhand, out_format)
-for pair in filtered_pairs:
-    write_seqs(pair, sys.stdout)
-sys.stdout.flush()
-'''
-
-
-def _unique_and_to_pairs(in_fhand, file_format):
-    prev_pair = None
-    for pair_line in in_fhand:
-        if prev_pair == None:
-            duplicated = False
-        else:
-            duplicated = _tabbed_pairs_equal(pair_line, prev_pair)
-        if not duplicated:
-            yield _tabbed_pair_to_seqs_seqitem(pair_line,
-                                               file_format=file_format)
-        prev_pair = pair_line
-
-
-def filter_duplicates(in_fhands, out_fhand, paired_reads, out_format='fastq'):
-    '''It filters exact duplicated sequences even with different
-    qualities or names. The output is given in fastq format by default,
-    but allows also fasta format'''
+def filter_duplicates(in_fhands, out_fhand, paired_reads, n_seqs_packet=None,
+                      directory=None):
     if not in_fhands:
         raise ValueError('At least one input fhand is required')
-    sort_cmd = ['sort']
-    if paired_reads:
-        if 'fastq' in get_format(in_fhands[0]):
-            keys = ['2,2', '5,5']
-        else:
-            keys = ['2,2', '4,4']
-    else:
-        keys = ['2,2']
-    for key in keys:
-        sort_cmd.append('-k')
-        sort_cmd.append(key)
-    sort_cmd.extend(['-T', '-u', '-t', "$'\t'"])
-    sort_cmd = ' '.join(sort_cmd)
-    sort = Popen(['/bin/bash', '-c', sort_cmd], stdin=PIPE, stdout=PIPE)
-
-    uniq_and_to_fastq_script = NamedTemporaryFile()
-    uniq_and_to_fastq_script.write(UNIQUE_SEQITEM_SCRIPT)
-    uniq_and_to_fastq_script.flush()
-    cmd = [sys.executable, uniq_and_to_fastq_script.name, str(out_format)]
-    uniq_and_to_fastq = Popen(cmd, stdin=sort.stdout, stdout=out_fhand)
-
-    for line_pair in _convert_fastq_to_tabbed_pairs(in_fhands, paired_reads):
-        sort.stdin.write(line_pair)
-    sort.stdin.close()
-    sort.wait()
-    uniq_and_to_fastq.wait()
-    uniq_and_to_fastq_script.close()
-    assert not sort.returncode
-    assert not uniq_and_to_fastq.returncode
+    pairs = _read_pairs(in_fhands, paired_reads)
+    for pair in sorted_unique_items(pairs, key=_key,
+                                    max_items_in_memory=n_seqs_packet,
+                                    temp_dir=directory):
+        write_seqs(pair, out_fhand)
+    out_fhand.flush()
--- test/test_bulk_filters.py
+++ test/test_bulk_filters.py
@@ -21,18 +21,15 @@
 
 import unittest
 import os
+
 from subprocess import check_output
 from tempfile import NamedTemporaryFile
 from StringIO import StringIO
 
-from crumbs.seq import (SeqWrapper, SeqItem, get_title,
-                        get_str_seq, get_str_qualities)
+from crumbs.seq import SeqWrapper, SeqItem
 from crumbs.utils.tags import SEQITEM
-from crumbs.bulk_filters import (filter_duplicates, _tabbed_pairs_equal,
-                                 _read_pairs, _convert_fastq_to_tabbed_pairs,
-                                 _tabbed_pair_to_seqs_seqitem,
-                                 _pair_to_tabbed_str, _seqitem_pairs_equal,
-                                 _unique_and_to_pairs)
+from crumbs.bulk_filters import (filter_duplicates, _read_pairs,
+                                 _seqitem_pairs_equal)
 from crumbs.utils.bin_utils import BIN_DIR
 
 FASTQ_NO_DUPS1 = '''@CUESXEL822 1:Y:18:ATCACG
@@ -95,24 +92,6 @@ CGCTCTTCTTCGCCACCGTGTTCTTGATATCGGCCTTC
 +
 ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
 '''
-TABBED_PAIR1 = '@CUESXEL836 1:Y:18:ATCACG\tTAATACACCCAGTCTCAATTCCATCCTGGGAACTA'
-TABBED_PAIR1 += 'AGT\tTTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\t@CUESXEL836 2:Y:'
-TABBED_PAIR1 += '18:ATCACG\tTCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\tABCBEGGGGF'
-TABBED_PAIR1 += 'GGGGGGGGGGGGGGGGBGGGA<EE=515\n'
-TABBED_PAIR2 = '@CUESXEL834 1:Y:18:ATCACG\tTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-TABBED_PAIR2 += 'TTT\tCG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\t@CUESXEL834 2:Y:'
-TABBED_PAIR2 += '18:ATCACG\tAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\tCG?BEGGGGF'
-TABBED_PAIR2 += 'GGGGGGGGGGGGGGGGBGGGA<EE=515\n'
-
-TABBED_SEQ1 = '@CUESXEL836 1:Y:18:ATCACG\tTAATACACCCAGTCTCAATTCCATCCTGGGAACTA'
-TABBED_SEQ1 += 'AGT\tTTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\n'
-TABBED_SEQ2 = '@CUESXEL836 2:Y:18:ATCACG\tTCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCC'
-TABBED_SEQ2 += 'TTC\tABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n'
-TABBED_SEQ3 = '@CUESXEL834 1:Y:18:ATCACG\tTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT'
-TABBED_SEQ3 += 'TTT\tCG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n'
-TABBED_SEQ4 = '@CUESXEL834 2:Y:18:ATCACG\tAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-TABBED_SEQ4 += 'AAA\tCG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n'
-
 FASTQ_PAIR = '''@CUESXEL834 1:Y:18:ATCACG
 TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT
 +
@@ -122,21 +101,18 @@ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
 +
 CG?BEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515
 '''
-TABBED_PAIR1_FASTA = '>CUESXEL836 1:Y:18:ATCACG\tTAATACACCCAGTCTCAATTCCATCCTGG'
-TABBED_PAIR1_FASTA += 'GAACTAAGT\t>CUESXEL836 2:Y:18:ATCACG\tTCATTACGTAGCTCCGG'
-TABBED_PAIR1_FASTA += 'CTCCGCCATGTCTGTTCCTTC\n'
-TABBED_SEQ1_FASTA = '>CUESXEL836 1:Y:18:ATCACG\tTAATACACCCAGTCTCAATTCCATCCTGGG'
-TABBED_SEQ1_FASTA += 'AACTAAGT\n'
 
 
-def _test_filter_duplicates(paired_reads):
+def _test_filter_duplicates(paired_reads, n_seqs_packet):
+    assert isinstance(n_seqs_packet, int) or n_seqs_packet == None
     in_fhand = NamedTemporaryFile()
     fastq_with_dups = (FASTQ_NO_DUPS1 + FASTQ_DUPS + FASTQ_NO_DUPS2
                        + FASTQ_NO_DUPS3)
     in_fhand.write(fastq_with_dups)
     in_fhand.flush()
+    in_fhand = open(in_fhand.name)
     out_fhand = NamedTemporaryFile()
-    filter_duplicates([in_fhand], out_fhand, paired_reads)
+    filter_duplicates([in_fhand], out_fhand, paired_reads, n_seqs_packet)
     filtered_pairs = list(_read_pairs([open(out_fhand.name)],
                                       paired_reads))
     fastq_no_dups = FASTQ_NO_DUPS1 + FASTQ_NO_DUPS2 + FASTQ_NO_DUPS3
@@ -144,6 +120,7 @@ def _test_filter_duplicates(paired_reads):
                                         paired_reads))
     #print 'filtered_pairs ->', filtered_pairs
     #print 'expected_pairs ->', expected_pairs
+    #print len(filtered_pairs), len(expected_pairs)
     #assert len(filtered_pairs) == len(expected_pairs)
     for pair1 in expected_pairs:
         counts = 0
@@ -154,110 +131,7 @@ def _test_filter_duplicates(paired_reads):
     in_fhand.close()
 
 
-def _test_fastq_to_tabbed_pairs(paired_reads):
-    fastq1 = FASTQ_DUPS
-    fastq2 = FASTQ_PAIR
-    in_fhand1 = StringIO(fastq1)
-    in_fhand2 = StringIO(fastq2)
-    lines = list(_convert_fastq_to_tabbed_pairs([in_fhand1, in_fhand2],
-                                                    paired_reads))
-    in_fhand1.seek(0)
-    lines2 = list(_convert_fastq_to_tabbed_pairs([in_fhand1],
-                                                     paired_reads))
-
-    expected_paired_lines = [TABBED_PAIR1, TABBED_PAIR2]
-    expected_single_lines = [TABBED_SEQ1, TABBED_SEQ2, TABBED_SEQ3,
-                             TABBED_SEQ4]
-    expected_lines = {True: expected_paired_lines,
-                      False: expected_single_lines}
-
-    assert len(lines) == len(expected_lines[paired_reads])
-    for i in range(len(lines)):
-        assert lines[i].strip() == expected_lines[paired_reads][i].strip()
-    for i in range(len(lines2)):
-        assert lines2[i].strip() == expected_lines[paired_reads][i].strip()
-
-
-def _test_pair_to_tabbed_str(paired_reads):
-    expected_tabbed_paired_pair = TABBED_PAIR1.rstrip()
-    expected_tabbed_single_pair = TABBED_SEQ1.rstrip()
-    expected_tabbed_pair = {True: expected_tabbed_paired_pair,
-                            False: expected_tabbed_single_pair}
-    seq1 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
-                        ['@CUESXEL836 1:Y:18:ATCACG\n',
-                        'TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\n', '+\n',
-                        'TTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\n']),
-                        'fastq')
-    seq2 = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
-                        ['@CUESXEL836 2:Y:18:ATCACG\n',
-                        'TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\n', '+\n',
-                        'ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n']),
-                        'fastq')
-    pair = {True: (seq1, seq2), False: [seq1]}
-    tabbed_pair = _pair_to_tabbed_str(pair[paired_reads]).rstrip()
-    #print tabbed_pair
-    #print expected_tabbed_pair
-    assert expected_tabbed_pair[paired_reads] == tabbed_pair
-
-
-def _test_tabbed_pair_to_seqitem(paired_reads, file_format):
-    tabbed_paired_pair = {'fastq': TABBED_PAIR1.rstrip(),
-                                   'fasta': TABBED_PAIR1_FASTA.rstrip()}
-    tabbed_single_pair = {'fastq': TABBED_SEQ1.rstrip(),
-                                   'fasta': TABBED_SEQ1_FASTA.strip()}
-    tabbed_pair = {True: tabbed_paired_pair, False: tabbed_single_pair}
-    seq1_fastq = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
-                        ['@CUESXEL836 1:Y:18:ATCACG\n',
-                        'TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\n', '+\n',
-                        'TTTDFG5GGEGGF;EGD=D@>GCCGFFGGGCECFE:D@\n']),
-                        'fastq')
-    seq2_fastq = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
-                        ['@CUESXEL836 2:Y:18:ATCACG\n',
-                        'TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\n', '+\n',
-                        'ABCBEGGGGFGGGGGGGGGGGGGGGGBGGGA<EE=515\n']),
-                        'fastq')
-    seq1_fasta = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
-                        ['@CUESXEL836 1:Y:18:ATCACG\n',
-                        'TAATACACCCAGTCTCAATTCCATCCTGGGAACTAAGT\n']),
-                        'fasta')
-    seq2_fasta = SeqWrapper(SEQITEM, SeqItem('CUESXEL836',
-                        ['@CUESXEL836 2:Y:18:ATCACG\n',
-                        'TCATTACGTAGCTCCGGCTCCGCCATGTCTGTTCCTTC\n']),
-                        'fasta')
-    expected_seqs = {True: {'fastq': [seq1_fastq, seq2_fastq],
-                     'fasta': [seq1_fasta, seq2_fasta]},
-                     False: {'fastq': [seq1_fastq],
-                     'fasta': [seq1_fasta]}}
-
-    seqs = _tabbed_pair_to_seqs_seqitem(tabbed_pair[paired_reads][file_format],
-                                        file_format)
-    for read1, read2 in zip(seqs, expected_seqs[paired_reads][file_format]):
-        assert get_title(read1) == get_title(read2)
-        assert get_str_seq(read1) == get_str_seq(read2)
-        if 'fastq' in file_format:
-            assert get_str_qualities(read1) == get_str_qualities(read2)
-
-
 class FilterDuplicatesTest(unittest.TestCase):
-
-    def test_tabbed_pairs_equal(self):
-        seq1 = '@seq1\tTAATAC\tTTTDFG'
-        seq2 = '@seq2\tTCATTA\tABCBEG'
-        seq3 = '@seq3\tTAATAC\tAGKDFG'
-        seq4 = '@seq4\tACGCGT\tASDJFG'
-
-        pair1 = '\t'.join([seq1, seq2])
-        pair2 = '\t'.join([seq2, seq4])
-        pair3 = '\t'.join([seq3, seq2])
-        pair4 = '\t'.join([seq2, seq1])
-
-        assert _tabbed_pairs_equal(pair1, pair3)
-        assert _tabbed_pairs_equal(pair1, pair2) == False
-        assert _tabbed_pairs_equal(pair1, pair4) == False
-        assert _tabbed_pairs_equal(seq1, seq3)
-        assert _tabbed_pairs_equal(seq1, seq2) == False
-        assert _tabbed_pairs_equal(seq1, pair1) == False
-        assert _tabbed_pairs_equal(pair1, seq2) == False
 
     def test_seqitem_pairs_equal(self):
         seq1 = SeqWrapper(SEQITEM, SeqItem('seq1',
@@ -285,79 +159,17 @@ class FilterDuplicatesTest(unittest.TestCase):
         assert _seqitem_pairs_equal([seq1], pair1) == False
         assert _seqitem_pairs_equal(pair1, seq2) == False
 
-    def test_pair_to_tabbed_str(self):
-        for option in [True, False]:
-            _test_pair_to_tabbed_str(option)
-
-    def test_fastq_to_tabbed_pairs(self):
-        for option in [True, False]:
-            _test_fastq_to_tabbed_pairs(option)
-
-    def test_tabbed_pair_to_seqitem(self):
-        for option in [True, False]:
-            for file_format in ['fastq', 'fasta']:
-                _test_tabbed_pair_to_seqitem(option, file_format)
-
-    def test_unique_and_to_pairs(self):
-        seq1 = '@seq1\tTAATAC\tTTTDFG'
-        seq2 = '@seq2\tTCATTA\tABCBEG'
-        seq3 = '@seq3\tTAATAC\tAGKDFG'
-        seq4 = '@seq4\tACGCGT\tASDJFG'
-
-        pair1 = '\t'.join([seq1, seq2])
-        pair2 = '\t'.join([seq2, seq4])
-        pair3 = '\t'.join([seq3, seq2])
-        pair4 = '\t'.join([seq2, seq1])
-        in_pairs = [pair1, pair3, pair2, pair4]
-        sorted_pairs = '\n'.join(in_pairs)
-        sorted_pairs2 = '\n'.join([seq4, seq3, seq1, seq2])
-        in_fhand = StringIO(sorted_pairs)
-        in_fhand2 = StringIO(sorted_pairs2)
-        filtered_pairs = list(_unique_and_to_pairs(in_fhand, 'fastq'))
-        filtered_pairs2 = list(_unique_and_to_pairs(in_fhand2, 'fastq'))
-        seq1 = SeqWrapper(SEQITEM, SeqItem('seq1',
-                            ['@seq1\n', 'TAATAC\n', '+\n',
-                             'TTTDFG\n']), 'fastq')
-        seq2 = SeqWrapper(SEQITEM, SeqItem('seq2',
-                            ['@seq2\n', 'TCATTA\n', '+\n',
-                             'ABCBEG\n']), 'fastq')
-        seq3 = SeqWrapper(SEQITEM, SeqItem('seq3',
-                            ['@seq3\n', 'TAATAC\n', '+\n',
-                             'TTTDFG\n']), 'fastq')
-        seq4 = SeqWrapper(SEQITEM, SeqItem('seq4',
-                            ['@seq4\n', 'ACGCGT\n', '+\n',
-                             'ABCBEG\n']), 'fastq')
-        pair1 = (seq1, seq2)
-        pair2 = (seq2, seq4)
-        pair3 = (seq3, seq2)
-        pair4 = (seq2, seq1)
-        expected_pairs = [pair1, pair2, pair4]
-        expected_pairs2 = [[seq3], [seq2], [seq4]]
-
-        assert len(expected_pairs2) == len(filtered_pairs2)
-        for pair1 in expected_pairs2:
-            counts = 0
-            for pair2 in filtered_pairs2:
-                if _seqitem_pairs_equal(pair1, pair2):
-                    counts += 1
-            assert counts == 1
-        in_fhand2.close()
-
-        assert len(expected_pairs) == len(filtered_pairs)
-        for pair1 in expected_pairs:
-            counts = 0
-            for pair2 in filtered_pairs:
-                if _seqitem_pairs_equal(pair1, pair2):
-                    counts += 1
-            assert counts == 1
-        in_fhand.close()
-
     def test_filter_duplicates(self):
-        _test_filter_duplicates(paired_reads=True)
-        _test_filter_duplicates(paired_reads=False)
+        options1 = [True, False]
+        options2 = [None, 3]
+        for option1 in options1:
+            for option2 in options2:
+                _test_filter_duplicates(paired_reads=option1,
+                                        n_seqs_packet=option2)
 
     def test_dup_bin(self):
-        seqs = '>seq1.f\naaa\n>seq1.r\naaa\n>seq2.f\naab\n>seq2.r\naaa\n'
+        seqs = '>seq1.f\naaa\n+\nHHHH\n>seq1.r\naaa\n+\nHHHH\n'
+        seqs += '>seq2.f\naab\n+\nHHHH\n>seq2.r\naaa\n+\nHHHH\n'
         in_fhand = NamedTemporaryFile()
         in_fhand.write(seqs)
         in_fhand.flush()
@@ -365,11 +177,18 @@ class FilterDuplicatesTest(unittest.TestCase):
         filter_bin = os.path.join(BIN_DIR, 'filter_duplicates')
         assert 'usage' in check_output([filter_bin, '-h'])
         result = check_output([filter_bin, in_fhand.name])
-        assert'>seq1.f\naaa\n>seq2.f\naab' in result
+        assert'>seq1.f\naaa\n+\nHHHH\n>seq2.f\naab\n+\nHHHH\n' in result
+        result = check_output([filter_bin], stdin=in_fhand)
+        assert'>seq1.f\naaa\n+\nHHHH\n>seq2.f\naab\n+\nHHHH\n' in result
+        result = check_output([filter_bin, in_fhand.name, '-d',
+                               '/home/carlos/devel/tmp'])
+        assert'>seq1.f\naaa\n+\nHHHH\n>seq2.f\naab\n+\nHHHH\n' in result
+        result = check_output([filter_bin, in_fhand.name, '-m', '3'])
+        assert'>seq1.f\naaa\n+\nHHHH\n>seq2.f\naab\n+\nHHHH\n' in result
         result = check_output([filter_bin, in_fhand.name, '--paired_reads'])
         assert seqs in result
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'FilterDuplicatesTest.test_filter_duplicates']
+    #import sys;sys.argv = ['', 'FilterDuplicatesTest.test_dup_bin']
     unittest.main()
