08a1a2052a8a3b4b8ecf4c8ee7ce64551a9e21a8
Jose Blanca <jblanca@upv.es>
2014-06-02 10:18:52 +0200

    Unused import removed

--- crumbs/seq.py
+++ crumbs/seq.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with seq_crumbs. If not, see <http://www.gnu.org/licenses/>.
 
-import itertools
 from copy import deepcopy
 from collections import namedtuple
 
