35765c68593571b25b9fa3c8c3fbc53265eaa4de
Jose Blanca <jblanca@upv.es>
2014-07-17 11:48:32 +0200

    PeekableIterator implemented

--- test/test_iterutils.py
+++ test/test_iterutils.py
@@ -1,7 +1,7 @@
 
 import unittest
 
-from vcf_crumbs.iterutils import generate_windows
+from vcf_crumbs.iterutils import generate_windows, PeekableIterator
 
 # Method could be a function
 # pylint: disable=R0201
@@ -26,6 +26,22 @@ class GenerateWindowsTests(unittest.TestCase):
         assert self.generate_wins(10, None, 3) == [(0, 10), (10, 20), (20, 30)]
         assert self.generate_wins(10, 5, 3) == [(0, 10), (5, 15), (10, 20)]
 
+    def test_peek(self):
+        stream = iter(range(5))
+        stream = PeekableIterator(stream)
+        assert stream.peek() == 0
+        assert stream.next() == 0
+        assert stream.next() == 1
+        assert stream.peek() == 2
+        assert stream.next() == 2
+        assert stream.next() == 3
+        assert stream.next() == 4
+        try:
+            stream.peek()
+            self.fail('StopIteration expected')
+        except StopIteration:
+            pass
+
 if __name__ == '__main__':
-    #import sys; sys.argv = ['', 'TrimChimericRegions']
+    # import sys; sys.argv = ['', 'TrimChimericRegions']
     unittest.main()
--- vcf_crumbs/iterutils.py
+++ vcf_crumbs/iterutils.py
@@ -11,3 +11,25 @@ def generate_windows(size, step=None, start=0):
             win_start += step
         win_end = win_start + size
         yield win_start, win_end
+
+
+class PeekableIterator(object):
+    def __init__(self, iterable):
+        self._stream = iterable
+        self._buffer = []
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self._buffer:
+            return self._buffer.pop()
+        return self._stream.next()
+
+    def peek(self):
+        try:
+            item = self._stream.next()
+        except StopIteration:
+            raise
+        self._buffer.append(item)
+        return item
