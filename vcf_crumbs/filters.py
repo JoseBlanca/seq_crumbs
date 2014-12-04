cd26b79528b1f4168129c3dcf74d2f3f561d141e
Jose Blanca <jblanca@upv.es>
2014-12-04 16:47:07 +0100

    filter snps by weird recombination implemented

--- vcf_crumbs/filters.py
+++ vcf_crumbs/filters.py
@@ -2,6 +2,20 @@ from __future__ import division
 
 import random
 from collections import OrderedDict
+from math import isnan
+from os.path import join as pjoin
+from array import array
+
+import numpy
+from pandas import DataFrame
+
+from scipy.optimize import curve_fit
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+from vcf import Reader as pyvcfReader
+from vcf import Writer as pyvcfWriter
 
 from crumbs.iterutils import group_in_packets
 
@@ -360,3 +374,227 @@ def filter_snvs_by_non_consistent_segregation(vcf_fpath, alpha=0.01,
         else:
             if passed:
                 yield snv_1
+
+
+def _get_chrom_lens(vcf_fpath):
+    reader = pyvcfReader(vcf_fpath)
+    chrom_lens = {}
+    for line in reader._reader:
+        if line[0] == '#':
+            continue
+        chrom, loc = line.split()[:2]
+        loc = int(loc)
+        if chrom_lens.get(chrom, 0) < loc:
+            chrom_lens[chrom] = loc
+    return chrom_lens
+
+
+def _get_calls(snv, samples):
+    if samples is None:
+        calls = snv.samples
+    else:
+        calls = [call for call in snv.samples if call.sample in samples]
+    return calls
+
+
+def _calculate_segregation_matrices(fpath, pop_type, samples=None,
+                                    max_snps_per_chrom_debug=None):
+    chrom_lens = _get_chrom_lens(open(fpath))
+
+    reader = pyvcfReader(open(fpath))
+
+    for chrom, length in chrom_lens.items():
+        snvs_in_chrom = list(reader.fetch(chrom, start=0, end=length + 1))
+        if max_snps_per_chrom_debug is not None:
+            snvs_in_chrom = snvs_in_chrom[:max_snps_per_chrom_debug]
+        calls = [_get_calls(snv, samples) for snv in snvs_in_chrom]
+        locs = [snv.POS for snv in snvs_in_chrom]
+        prev_chrom = chrom
+        rates = []
+        for snv1_calls in calls:
+            snv1_rates = []
+            for snv2_calls in calls:
+                recomb_rate = _calc_recomb_rate(snv1_calls, snv2_calls,
+                                                pop_type)
+                if recomb_rate is None:
+                    recomb_rate = float('nan')
+                else:
+                    recomb_rate = recomb_rate[0]
+                snv1_rates.append(recomb_rate)
+            rates.append(snv1_rates)
+        yield chrom, DataFrame(rates, index=locs, columns=locs)
+
+
+def _kosambi(phys_dist, phys_gen_dist_conversion, recomb_at_origin):
+    phys_gen_dist_conversion = abs(phys_gen_dist_conversion)
+    # recomb rate should be in morgans per base
+    d4 = numpy.absolute(phys_dist) * phys_gen_dist_conversion * 4
+    return 0.5 * (numpy.exp(d4) - 1)/ (numpy.exp(d4) + 1) + recomb_at_origin
+
+
+def _fit_kosambi(dists, recombs, init_params):
+    try:
+        return curve_fit(_kosambi, dists, recombs, p0=init_params)[0]
+    except RuntimeError:
+        return
+
+
+def _calc_ajusted_recomb(dists, recombs, max_recomb, plot_fhand=None):
+    # first rough interpolation
+    # we remove the physical distances with high recombination rates because
+    # they're not very informative. e.g. more than 40 cM will not discriminate
+    # between false recombination due to hidden segregation in the parents and
+    # true recombination
+
+    dists = numpy.array(dists)
+    recombs = numpy.array(recombs)
+    recomb_rate = 1e-7
+    popt = _fit_kosambi(dists, recombs, init_params=[recomb_rate, 0])
+    if popt is None:
+        return float('nan')
+
+    est_dists = dists
+    est_recombs = _kosambi(est_dists, popt[0], popt[1])
+
+    #now we perform a second fit but only with those markers that are a
+    #distance that results in a recombination fraction lower than max_recomb
+    # TODO use the numpy comparison
+    close_markers = [idx for idx, est_recomb in enumerate(est_recombs) if est_recomb < max_recomb]
+    close_recombs = recombs[close_markers]
+    close_dists = dists[close_markers]
+    if len(close_dists) < 1:
+        # This marker is so bad that their closest markers are at a big
+        # distance
+        return float('nan')
+
+    popt = _fit_kosambi(close_dists, close_recombs, init_params=popt)
+    if popt is None:
+        return float('nan')
+
+    est_close_recombs = _kosambi(close_dists, popt[0], popt[1])
+
+    residuals = close_recombs - est_close_recombs
+
+    # we exclude the markers with a residual outlier
+    quartile_25, quartile_75 = numpy.percentile(residuals, [25 ,75])
+    iqr = quartile_75 - quartile_25
+    outlayer_thrld = [quartile_25 - iqr * 1.5, quartile_75 + iqr * 1.5]
+    ok_markers = ((outlayer_thrld[0] < residuals) &
+                  (residuals < outlayer_thrld[1]))
+    ok_recombs = close_recombs[ok_markers]
+    ok_dists = close_dists[ok_markers]
+
+    popt = _fit_kosambi(ok_dists, ok_recombs, init_params=popt)
+    if popt is None:
+        return float('nan')
+
+    est2_recombs = _kosambi(ok_dists, popt[0], popt[1])
+
+    if plot_fhand:
+        fig = Figure()
+        axes = fig.add_subplot(111)
+
+        axes.scatter(dists, recombs, c='r')
+        axes.scatter(ok_dists, ok_recombs, c='b')
+        axes.plot(close_dists, est_close_recombs)
+        axes.plot(ok_dists, est2_recombs)
+        canvas = FigureCanvas(fig)
+        canvas.print_figure(plot_fhand)
+
+    return popt[1]
+
+
+class _WeirdRecombFilter(object):
+    def __init__(self, min_num_snps, max_zero_dist_recomb,
+                 max_recomb_curve_fit, debug_plot_dir):
+
+        self.min_num_snps = min_num_snps
+        self.max_recomb_curve_fit = max_recomb_curve_fit
+        self.max_zero_dist_recomb = max_zero_dist_recomb
+        self.debug_plot_dir = debug_plot_dir        
+        if debug_plot_dir:
+            fpath = pjoin(debug_plot_dir, 'dist_zero_recombs.png')
+            dist_zero_recomb_distrib_fhand = open(fpath, 'w')
+            self.dist_zero_recomb_distrib_fhand = dist_zero_recomb_distrib_fhand
+            self.all_recombs = array('f')
+        else:
+            self.dist_zero_recomb_distrib_fhand = None
+            self.all_recombs = None
+        self.snp_index = 0
+
+    def filter_snps_weird_recom(self, chrom, matrix):
+
+        all_recombs = self.all_recombs
+
+        locations = matrix.columns
+
+        if len(locations) < self.min_num_snps:
+            yield self.snp_index, False
+
+        debug_plot_dir = self.debug_plot_dir
+        max_recomb = self.max_recomb_curve_fit
+        max_zero_dist_recomb = self.max_zero_dist_recomb
+        for pos, recombs in matrix.iteritems():
+            if debug_plot_dir:
+                fpath = pjoin(debug_plot_dir, chrom + str(pos) + '.png')
+                plot_fhand = open(fpath, 'w')
+            else:
+                plot_fhand = None
+
+            dists = [loc - pos for loc in locations]
+            recomb_dist_zero = _calc_ajusted_recomb(dists, recombs, max_recomb,
+                                                    plot_fhand=plot_fhand)
+
+            # TODO find the method to decide the thresholds
+            filter_result = recomb_dist_zero < max_zero_dist_recomb
+
+            yield self.snp_index, filter_result
+
+            if all_recombs is not None and not isnan(recomb_dist_zero):
+                all_recombs.append(recomb_dist_zero)
+
+            self.snp_index += 1
+
+    def plot_recombs_distrib(self, bins=40, log_axis=True):
+        if self.debug_plot_dir:
+            fig = Figure()
+            axes = fig.add_subplot(111)
+            axes.hist(all_recombs, bins=bins, log=log_axis)
+                      #range=range(min(all_recombs, max(all_recombs))))
+            canvas = FigureCanvas(fig)
+            canvas.print_figure(self.dist_zero_recomb_distrib_fhand)
+
+
+def filter_snps_weird_recomb(vcf_fpath, out_vcf_fpath, pop_type,
+                             max_zero_dist_recomb, min_num_snps=20,
+                             max_recomb_curve_fit=0.25,
+                             debug_plot_dir=None):
+
+    # TODO max number of markers?
+    # TODO The matrix is symmetric, DUDE!!!
+    # TODO Add the tests
+
+    recomb_filter = _WeirdRecombFilter(min_num_snps,
+                                     max_zero_dist_recomb=max_zero_dist_recomb,
+                                     max_recomb_curve_fit=max_recomb_curve_fit,
+                                     debug_plot_dir=debug_plot_dir)
+
+    snp_reader = pyvcfReader(open(vcf_fpath))
+    snp_writer = pyvcfWriter(open(out_vcf_fpath, 'w'), template=snp_reader)
+    for chrom, matrix in _calculate_segregation_matrices(vcf_fpath,
+                                                         pop_type=pop_type):
+        for keepit in recomb_filter.filter_snps_weird_recom(chrom, matrix):
+            try:
+                if keepit:
+                    snp = snp_reader.next()
+                else:
+                    # Just the next line, we can skip parsing the snp because
+                    # we don't need it
+                    snp_reader.reader.next()
+            except StopIteration:
+                msg = 'Fixme, we should not be here'
+                raise RuntimeError(msg)
+            if keepit:
+                snp_writer.write_record(snp)
+    
+    recomb_filter.plot_recombs_distrib()
